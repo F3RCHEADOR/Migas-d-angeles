2a3995693a2eede08d1418b9bd3750e45ead7691
Fabian F <noobseca123@hotmail.com>
2024-11-22 16:33:57 -0500

    responsive

--- src/components/ui/SliderEventoImagen.jsx
+++ src/components/ui/SliderEventoImagen.jsx
@@ -33,11 +33,11 @@ const SliderEventoImagen = () => {
     }, []); // Se pasa un array vacío para ejecutar el efecto una sola vez al montar el componente
 
     return (
-        <div className="relative w-4/5 h-[80dvh] mx-auto">
+        <div className="relative w-full md:w-4/5 h-[80dvh] mx-auto">
             <img
                 src={bgImages[currentImageIndex].src}
                 alt={bgImages[currentImageIndex].alt}
-                className={`w-full h-full shadow-lg  rounded-md object-cover absolute transition-opacity duration-1000 ${
+                className={`w-full h-3/4 md:h-full shadow-lg  rounded-md object-cover absolute transition-opacity duration-1000 ${
                     fade ? 'opacity-100' : 'opacity-0'
                 }`}
             />
